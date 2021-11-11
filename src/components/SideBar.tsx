826a559126875bb3f2922e3b232ee2dd7517b72a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-11 13:43:14 -0300

    feat: solve exercise

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,42 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+
+import { Button } from './Button';
+import { api } from '../services/api';
+
+import { GenreResponseProps, Genres } from '../@types';
+
+export default function SideBar(props: GenreResponseProps) {
+  const { selectedGenreId, setSelectedGenreId } = props;
+
+  const [genres, setGenres] = useState<Genres[]>([]);
+
+  useEffect(() => {
+    api.get<Genres[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
